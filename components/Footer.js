b897515726cce3619b77125beca5bfd0789a91a1
PeshoBiceps <peshowww@gmail.com>
2021-09-15 18:49:15 +0300

    .

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+const Footer = () => {
+  return (
+    <div className='grid grid-cols-2 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>News room</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>COMMUNITY</h5>
+        <p>Accessability</p>
+        <p>This is not a real site</p>
+        <p>Awesome Clone</p>
+        <p>Peshowww</p>
+        <p>Refs</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>HOST</h5>
+        <p>React</p>
+        <p>Tailwind</p>
+        <p>Say Hi </p>
+        <p>Join Now</p>
+        <p>Tosho Kukata</p>
+      </div>
+
+      <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>For the win</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
+import LargeCard from '../components/LargeCard'
+import MediumCard from '../components/MediumCard'
+import SmallCard from '../components/SmallCard'
 
-export default function Home() {
+export default function Home({ exploreData, cardData }) {
   return (
     <div>
       <Head>
@@ -10,7 +15,63 @@ export default function Home() {
       </Head>
 
       <Header />
-      {/* Banner */}
+      <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16' >
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* Pull some data form a server */}
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 '>
+            {exploreData?.map((data, index) => (
+              <SmallCard
+                key={index}
+                img={data.img}
+                distance={data.distance}
+                location={data.location}
+              />
+            ))}
+          </div>
+        </section>
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Live Anywhere</h2>
+          <div className='flex space-x-5 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {cardData?.map((data, index) => (
+              <MediumCard
+                key={index}
+                img={data.img}
+                title={data.title}
+              />
+            ))}
+          </div>
+        </section>
+
+        <LargeCard
+          img='https://a0.muscache.com/im/pictures/2da67c1c-0c61-4629-8798-1d4de1ac9291.jpg?im_w=1440'
+          title='The Greatest Outdoors'
+          description='Wishlists curated by Airbnb'
+          buttonText='Get Inspired'
+        />
+
+      </main>
+      <Footer />
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://jsonkeeper.com/b/4G1G').
+    then((res) => res.json())
+
+  const cardData = await fetch('https://jsonkeeper.com/b/VHHT').
+    then((res) => res.json())
+
+  return {
+    props: {
+      exploreData,
+      cardData
+    }
+  }
+
 }
